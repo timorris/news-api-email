70fe498934eb4dd1e3d72f7622a4569f34d51c47
Tim Morris <timorris@timorris.com>
2025-09-17 13:40:24 -0400

    fixed styling

--- main.py
+++ main.py
@@ -23,20 +23,21 @@ body = ""
 for article in content['articles'][:20]:
     if article['title'] is not None:
         body += rf"""
-        <div style="display:flex flex-direction:row;padding:5px;justify-content:center;align-items:center;">
-          <div style="width:100px;height:auto;border-radius:4px;overflow:hidden;margin-right:5px;display:flex:1">
-            {'<a href=' + article["url"] + ' style="color:#666;text-decoration:none;hover:underline;"><img src=' + article["urlToImage"] + ' alt=' + article["title"] + ' style="width:100px;height:auto;border border-radius:4px;" /></a>' if article['urlToImage'] is not None and not article["urlToImage"].endswith('.webp') else ''}
+        <div style="display:flex; flex-direction:row;width:100%;margin-bottom:10px;border-bottom:1px solid #666;padding:10px 0;">
+          <div style="flex: 1;">
+            {'<a href="' + article["url"] + '"><img src="' + article["urlToImage"] + '" alt="' + article["title"] + '" style="width:150px;height:auto;border-radius:5px;" /></a>' if article['urlToImage'] is not None and not article["urlToImage"].endswith('.webp') else ''}
           </div>
-          <div style="display:flex:1">
+          <div style="flex: 2;">
             <div>
                 <span style="color:#666;font-size:12px;"><em>Source: {article['source']['name']}: </em></span>
             </div>
-            <a href='{article["url"]}' style="color:#666;text-decoration:none;hover:underline;">
+            <a href="{article['url']}" style="color:blue;text-decoration:none;&hover:text-decoration:underline;">
              <b>{article['title']}</b>
-            </a><br/>
-            {article['description'] + "<br/>" if article['description'] is not None else ''}
+            </a>
+            <div style="margin-top:5px;">
+              {article['description'] if article['description'] is not None else ''}
+            </div>
           </div>
-          <hr/>
         </div>
     """
 
